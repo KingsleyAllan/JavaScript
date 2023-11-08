df07dc28d1ffcef2fe53191ec8fc5a2bad4efacb
KingsleyAllan <kingsleyallan125@gmail.com>
2023-11-08 19:56:23 +0300

    JavaScript

---
+++ Intro/JS.js
@@ -0,0 +1,14 @@
+alert("HTML CSS was easy");
+alert("PHP was confusing");
+alert("I'm just saying");
+alert("I'm back to zero");
+alert("that was not me speaking");
+alert("where's the console...I can't see it");
+alert("never have i ever...seen the console");
+alert("half of the class is gone");
+alert("Nicholus took them all and left me with console");
+alert("I'm done complaining");
+alert("Ha Ha..Just kidding")
+
+console.log("Don't mind me...I'm not complaining or anything like that");
+console.log(5+99999999995);
---
+++ app.js
@@ -0,0 +1,12 @@
+export function add(a, b) {
+    return a + b;
+}
+
+export function greet() {
+    console.log("hey");
+}
+
+// arrow function
+() => {
+    console.log("hey")
+}
